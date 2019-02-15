0a88cc4558e1500959c5c89cfc64c5c5cc08ab89
Alvaro Rocha <alvaro.rocha@fabritech.com>
2019-02-15 21:26:14 -0200

    initial

---
+++ blog1/Models/RegistroViewModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace blog.Models
+{
+    public class RegistroViewModel
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [Display(Name ="Nome")]
+        public string LoginName { get; set; }
+
+        [Required]
+        public string Email { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Senha { get; set; }
+
+        [Required]
+        [Compare("Senha")]
+        [DataType(DataType.Password)]
+        [Display(Name ="Confirmação da senha")]
+        public string ConfirmaSenha { get; set; }
+    }
+}
